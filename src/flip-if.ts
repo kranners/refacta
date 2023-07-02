5de160d4371677b82f72ace9a1cf10c0ef4a9560
Aaron Pierce <aaronpierce@outlook.com.au>
2023-07-02 15:01:02 +1000

    build: adding chai, license, updating readme for builds

---
+++ src/flip-if.ts
@@ -0,0 +1,20 @@
+import * as vscode from "vscode";
+
+export class IfFlipper implements vscode.CodeActionProvider {
+  public static readonly providedCodeActionKinds = [
+    vscode.CodeActionKind.QuickFix,
+  ];
+
+  provideCodeActions(
+    // document: vscode.TextDocument,
+    // range: vscode.Range | vscode.Selection,
+    // context: vscode.CodeActionContext,
+    // token: vscode.CancellationToken
+  ): vscode.ProviderResult<(vscode.CodeAction | vscode.Command)[]> {
+    throw new Error("not implemented");
+    const getSymbols = new vscode.CodeAction(
+      "get symbols",
+      vscode.CodeActionKind.QuickFix
+    );
+  }
+}
---
+++ src/test/suite/flip-if.test.ts
@@ -0,0 +1,3 @@
+describe('flip if code action', () => {
+
+});
